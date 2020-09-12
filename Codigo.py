26b94bf1d167801285d6ece3a5410f6d4a12e8f9
danymessi1000@gmail.com <danymessi1000@gmail.com>
2020-09-12 17:07:46 -0500

    Creo la clase

---
+++ Codigo.py
@@ -0,0 +1,12 @@
+class Prograteo:
+    
+    def __init__(self,materiaPrimaIndirecta,manoDeobraIndirecta,renta,energiaElectrica,agua,telefono,seguroDeEdificio,depreciacionDeMaquinaria,seguroDeMaquinaria):
+        self.__materiaPrimaIndirecta=materiaPrimaIndirecta
+        self.__manoDeobraIndirecta=manoDeobraIndirecta
+        self.__renta=renta
+        self.__energiaElectrica=energiaElectrica
+        self.__agua=agua
+        self.__telefono=telefono
+        self.__seguroDeEdificio=seguroDeEdificio
+        self.__depreciacionDeMaquinaria=depreciacionDeMaquinaria
+        self.__seguroDeMaquinaria=seguroDeMaquinaria
